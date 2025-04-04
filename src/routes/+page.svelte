3dfac1db9bb1b98f3467a0aeef99e93a033274a4
janithwanni <janithcwanni@gmail.com>
2025-04-04 20:52:54 +1100

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<h1>30 Day Chart challenge</h1>
+<div class="flex justify-center items-center flex-col h-screen w-screen">
 
-Site under construction
+<div class="font-playfair text-6xl"> 30 Day Chart Challenge 2025 </div>
+
+</div>
