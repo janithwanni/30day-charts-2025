a088f82f43f0c18d7812a8e6384958af12a47f69
janithwanni <janithcwanni@gmail.com>
2025-04-08 12:44:57 +1000

    typo

--- src/routes/day4/+page.svelte
+++ src/routes/day4/+page.svelte
@@ -11,7 +11,7 @@ let {data} = $props();
     <div class="font-winky py-4 text-xl">
     The <span style="color: #4269d0" class="font-italic">circles are plastic bottles </span> that were collected while the <span style="color: #efb118" class="font-italic">circles are the glass bottles </span> that were collected every day with <span class="font-bold">in the year 2022</span>
     </div>
-    <div class="pb-4 font-winky text-2xl font-bold italic">We have too many <span style="color: #efb118">glass bottles </span> thrown in the Harbor area around the Baltimore Area</div>
+    <div class="pb-4 font-winky text-2xl font-bold italic">We have too many <span style="color: #4269d0">plastic bottles </span> thrown in the Harbor area around the Baltimore Area</div>
 
 <!-- Going to use cluster forces to animate the number of items from different rubbish types -->
   </div>
