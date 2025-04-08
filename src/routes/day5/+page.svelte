b8a9662213ec41af0fd4036358c556bce6056e16
janithwanni <janithcwanni@gmail.com>
2025-04-08 11:00:21 +1000

    font change

--- src/routes/day5/+page.svelte
+++ src/routes/day5/+page.svelte
@@ -8,7 +8,7 @@ let {data} = $props();
 <div class="flex flex-col justify-center items-center p-8">
   <div class = "flex flex-col justify-center items-center">
     <div class = "text-4xl font-playfair font-bold py-2">Day 5: Ranking</div>
-    <div class="font-winky py-4 text-xl"> Description goes here</div>
+    <div class="font-winky py-4 text-xl"> We are going to look at International Math Olympiad contestant gender disparity data</div>
 <!-- We are going to use the IMO dataset and then show the ranks of each individual across years and put a slider to show how many of the top performing contestants are women across the years-->
   </div>
   <Ranking data={data.data} />
--- src/routes/day6/+page.svelte
+++ src/routes/day6/+page.svelte
@@ -8,7 +8,7 @@ let {data} = $props();
 <div class="flex flex-col justify-center items-center p-8">
   <div class = "flex flex-col justify-center items-center">
     <div class = "text-4xl font-playfair font-bold py-2">Day 6: Florence Nightingale</div>
-    <div class="font-winky py-4 text-xl"> Description goes here</div>
+    <div class="font-winky py-4 text-xl"> We are going to look at creating a sunburst graph</div>
 <!-- We are going to use the IMO dataset and then show the ranks of each individual across years and put a slider to show how many of the top performing contestants are women across the years-->
   </div>
   <Radial data={data.data} />
